fd01d197b024c1f3cee916a0f6cbc74a4c3cbd49
BearKS <56060226+BearKS@users.noreply.github.com>
2022-05-02 05:53:20 +0700

    add app.py

--- frontend/app.py
+++ frontend/app.py
@@ -4,67 +4,72 @@ from PIL import Image, ImageEnhance
 import numpy as np
 import os
 
-st.title("ทดลองใช้งาน")
-st.write("ffffff")
-st.write("fffffdddddf")
 @st.cache
 def load_image(img):
 	im = Image.open(img)
 	return im
 
 def main():
-	""" Face Detection App """
+    #Add a header and expander in side bar
+	st.sidebar.markdown('<p class="font">My First Photo Converter App</p>', unsafe_allow_html=True)
+	with st.sidebar.expander("About the App"):
+		st.write("""
+			Use this simple app to convert your favorite photo to a pencil sketch, a grayscale image or an image with blurring effect.  \n  \nThis app was created by Sharone Li as a side project to learn Streamlit and computer vision. Hope you enjoy!
+		""")
+		image = Image.open(r'./assets/logo.jpg') #Brand logo image (optional)
 
-	st.title(" Face Detection and Editing App")
-
-	activities = ["Detection","About"]
-	choice = st.sidebar.selectbox("Select Activity",activities)
-
-	if choice== 'Detection':
-		st.subheader("Face Detection")
-
-		image_file = st.file_uploader("Upload Image",type=['jpg','png','jpeg'])
-
-		if image_file is not None:
-			our_image = Image.open(image_file)
-			st.text("Original Image")
-			st.image(our_image)
-
-		enhance_type = st.sidebar.radio("Enhance Type",["Original","Gray-Scale",
-			"Contrast","Brightness","Blurring"])
-
-		if enhance_type== 'Gray-Scale':
-			new_img = np.array(our_image.convert('RGB'))
-			img = cv.cvtColor(new_img,1)
-			img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-			st.image(img)
-
-		if enhance_type== 'Contrast':
-			c_rate = st.sidebar.slider("Contrast",0.5,3.5)
-			enhancer = ImageEnhance.Contrast(our_image)
-			img_output = enhancer.enhance(c_rate)
-			st.image(img_output)
-
-		if enhance_type== 'Brightness':
-			c_rate = st.sidebar.slider("Brightness",0.5,3.5)
-			enhancer = ImageEnhance.Brightness(our_image)
-			img_output = enhancer.enhance(c_rate)
-			st.image(img_output)
-
-		if enhance_type== 'Blurring':
-			new_img = np.array(our_image.convert('RGB'))
-			blur_rate = st.sidebar.slider("Blur Rate",0.5,3.5)
-			img = cv.cvtColor(new_img,1)
-			img = cv.GaussianBlur(img,(5,5),blur_rate)
-			st.image(img)
-
-		"""Face Detection"""
-
-		task = ["Faces","Smiles","Eyes","Cannize","Cartoonize"]
-		feature_choice = st.sidebar.selectbox("Find Features",task)
-		if st.button("Process"):
-
+	#Create two columns with different width
+	col1, col2 = st.columns( [0.8, 0.2])
+	with col1:               # To display the header text using css style
+		st.markdown(""" <style> .font {
+		font-size:35px ; font-family: 'fantasy'; color: #FF9633;} 
+		</style> """, unsafe_allow_html=True)
+		st.markdown('<p class="font">Upload your photo here...</p>', unsafe_allow_html=True)
+		
+	with col2:               # To display brand logo
+		st.image(image,  width=150)
+	#Add file uploader to allow users to upload photos
+	uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
+ 
+	#Add 'before' and 'after' columns
+	if uploaded_file is not None:
+		image = Image.open(uploaded_file)
+		
+		col1, col2 = st.columns( [0.5, 0.5])
+		with col1:
+			st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
+			st.image(image,width=300)  
+	
+		#Add conditional statements to take the user input values
+		with col2:
+			st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
+			filter = st.sidebar.radio('Covert your photo to:', ['Original','Gray Image','Black and White', 'Pencil Sketch', 'Blur Effect'])
+			if filter == 'Gray Image':
+					converted_img = np.array(image.convert('RGB'))
+					gray_scale = cv.cvtColor(converted_img, cv.COLOR_RGB2GRAY)
+					st.image(gray_scale, width=300)
+			elif filter == 'Black and White':
+					converted_img = np.array(image.convert('RGB'))
+					gray_scale = cv.cvtColor(converted_img, cv.COLOR_RGB2GRAY)
+					slider = st.sidebar.slider('Adjust the intensity', 1, 255, 127, step=1)
+					(thresh, blackAndWhiteImage) = cv.threshold(gray_scale, slider, 255, cv.THRESH_BINARY)
+					st.image(blackAndWhiteImage, width=300)
+			elif filter == 'Pencil Sketch':
+					converted_img = np.array(image.convert('RGB')) 
+					gray_scale = cv.cvtColor(converted_img, cv.COLOR_RGB2GRAY)
+					inv_gray = 255 - gray_scale
+					slider = st.sidebar.slider('Adjust the intensity', 25, 255, 125, step=2)
+					blur_image = cv.GaussianBlur(inv_gray, (slider,slider), 0, 0)
+					sketch = cv.divide(gray_scale, 255 - blur_image, scale=256)
+					st.image(sketch, width=300) 
+			elif filter == 'Blur Effect':
+					converted_img = np.array(image.convert('RGB'))
+					slider = st.sidebar.slider('Adjust the intensity', 5, 81, 33, step=2)
+					converted_img = cv.cvtColor(converted_img, cv.COLOR_RGB2BGR)
+					blur_image = cv.GaussianBlur(converted_img, (slider,slider), 0, 0)
+					st.image(blur_image, channels='BGR', width=300) 
+			else: 
+					st.image(image, width=300)
 
 if __name__ == '__main__':
 	main()
- 
